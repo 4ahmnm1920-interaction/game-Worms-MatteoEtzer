8f239af682d5a08ddf30f8502a3726b0b79d1587
matteoetzer@gmx.at <matteoetzer@gmx.at>
2020-02-05 11:15:25 +0100

    shooting redone and two players

--- Assets/MyGame/Scripts/WormsController2.cs
+++ Assets/MyGame/Scripts/WormsController2.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WormsController : MonoBehaviour
+public class WormsController2 : MonoBehaviour
 {
     public Rigidbody rb;
     public float jumpHeight;
     public float movement;
-    public float laser;
-    public float ammospeed;
+    public Vector3 ammospeed;
     public Rigidbody projectile;
-    public Transform Spawnpoint;
+    public GameObject positionObject;
     void Start()
     {
         
@@ -18,45 +17,40 @@ public class WormsController : MonoBehaviour
 
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.Space))
+       if (Input.GetKeyDown(KeyCode.UpArrow))
        {
            Vector3 force = new Vector3(0, jumpHeight, 0);
            rb.AddForce(force, ForceMode.Impulse);
            Debug.Log("Die Leertaste wurde gedrückt");
        }
-       if (Input.GetKey(KeyCode.D))
+       if (Input.GetKey(KeyCode.RightArrow))
        {
            Vector3 move = new Vector3(movement, 0, 0);
            rb.AddForce(move, ForceMode.Force);
            Debug.Log("D wurde gedrückt");
        }
-       if (Input.GetKey(KeyCode.A))
+       if (Input.GetKey(KeyCode.LeftArrow))
        {
            Vector3 move = new Vector3(movement * (-1), 0, 0);
            rb.AddForce(move, ForceMode.Force);
            Debug.Log("A wurde gedrückt");
        }
-       if (Input.GetButtonDown("Fire1"))
+       if (Input.GetKeyDown(KeyCode.RightShift))
         {
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 3; i++)
             {
             Rigidbody clone;
-            clone = Instantiate(projectile, Spawnpoint.position, transform.rotation);
-            Vector3 pSpeed = new Vector3(ammospeed, 0, 0);
-            
-            clone.AddForce(pSpeed);
+            clone = Instantiate(projectile, transform.position, transform.rotation);
+            clone.velocity = transform.TransformDirection(Vector3.forward * 25);
             }
         }
-        if (Input.GetKeyDown(KeyCode.G))
+        if (Input.GetKeyDown(KeyCode.Asterisk))
         {
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < 1000; i++)
             {
             Rigidbody clone;
-            rb.mass = 1;
-            clone = Instantiate(projectile, Spawnpoint.position, transform.rotation);
-            Vector3 pSpeed = new Vector3(laser, 0, 0);
-            
-            clone.AddForce(pSpeed);
+            clone = Instantiate(projectile, transform.position, transform.rotation);
+            clone.velocity = transform.TransformDirection(Vector3.forward * 25);
             }
         }
     }
